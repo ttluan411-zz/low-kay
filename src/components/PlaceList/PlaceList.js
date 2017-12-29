1c24774bae7de91f1c7bd42359a2431026949f4d
Logan Truong <ttluan411@gmail.com>
2017-12-28 23:58:19 -0800

    added flatlist

--- src/components/PlaceList/PlaceList.js
+++ src/components/PlaceList/PlaceList.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import { View, StyleSheet, ScrollView } from 'react-native';
+import { StyleSheet, FlatList } from 'react-native';
 
 import ListItem from '../ListItem/ListItem';
 
 const placeList = props => {
-    const placesOutput = props.places.map((place, i) => (
-        <ListItem 
-        key={i} 
-        placeName={place} 
-        onItemPressed = {() => props.onItemDeleted(i)}
-        />
-      ));
     return (
-        <ScrollView style={styles.listContainer}>{placesOutput}</ScrollView>
-    );
+        <FlatList 
+        style={styles.listContainer}
+        data={props.places}
+        renderItem={(info) => (
+            <ListItem
+            placeName={info.item.value}
+            onItemPressed={() => props.onItemDeleted(info.item.key)}
+            />
+        )}
+        />
+    )
 };
 
 const styles = StyleSheet.create({
